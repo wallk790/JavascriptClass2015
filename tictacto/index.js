c08da418e9f840d2e9e541c7d90ddee7dc6a3189
Wallk790walclear <wallk790@newschool.edu>
2015-04-10 18:49:34 -0400

    AH THIS IS SO HARD

--- tictacto/index.js
+++ tictacto/index.js
@@ -8,82 +8,195 @@ var $button6 = $('.button-6');
 var $button7 = $('.button-7');
 var $button8 = $('.button-8');
 var $button9 = $('.button-9');
+var $button10 = $('.button-10');
+var $button11 = $('.button-11');
+var $text1 = $('.text1');
+var $text2 = $('.text2');
+var $text3 = $('.text3');
+var $text4 = $('.text4');
+var $text5 = $('.text5');
+var $text6 = $('.text6');
+var $text7 = $('.text7');
+var $text8 = $('.text8');
+var $text9 = $('.text9');
+
 
 //player button options 
-var buttonOption = ['button1', 'button2', 'button3', 'button4', 'button5', 'button6', 'button7', 'button8', 'button9'];
+var buttonOption = ['button1', 'button2', 'button3', 'button4', 'button5', 'button6', 'button7', 'button8', 'button9' , 'button10', 'button11'];
 var playerOption = '';
-var computerChoice = ''; 
+var player2Option = '';
 
+var player1Turn; 
 
-function buttonEvents(){
+//var computerChoice = ''; 
+
+function init() {
+
+    buttonEvents();
+    buttonEvents2;
+
+}
+
+function buttonEvents() {
+
+    $(".button").on('click', function(){
+        player1Turn=!player1Turn;
+        console.log("whose turn:" +player1Turn);
+    })
 	
 	$button1.on('click', function() {
+
 	playerOption = buttonOption[0];
-    computerChoice = makeComputerChoice();
+    //computerChoice = makeComputerChoice();
         console.log (playerOption);
-        console.log (computerChoice);
+        if  (player1Turn = true) {
+            $text1.text('X');
+        }
+        //console.log (computerChoice);
 	});
 
 	$button2.on('click', function() {
 	playerOption = buttonOption[1];
-    computerChoice = makeComputerChoice();
+    //computerChoice = makeComputerChoice();
         console.log (playerOption);
-        console.log (computerChoice);
+        if  (player1Turn = true) {
+            $text2.text('X');
+        }
+        //console.log (computerChoice);
 	});
 
 	$button3.on('click', function() {
 	playerOption = buttonOption[2];
-    computerChoice = makeComputerChoice();
+    //computerChoice = makeComputerChoice();
         console.log (playerOption);
-        console.log (computerChoice);
+        $text3.text('X');
+        //console.log (computerChoice);
 	});
 
 	$button4.on('click', function() {
 	playerOption = buttonOption[3];
-    computerChoice = makeComputerChoice();
+    //computerChoice = makeComputerChoice();
         console.log (playerOption);
-        console.log (computerChoice);
+        $text4.text('X');
+        //console.log (computerChoice);
 	});
 
 	$button5.on('click', function() {
 	playerOption = buttonOption[4];
-    computerChoice = makeComputerChoice();
+    //computerChoice = makeComputerChoice();
         console.log (playerOption);
-        console.log (computerChoice);
+        $text5.text('X');
+        //console.log (computerChoice);
 	});
 
 	$button6.on('click', function() {
 	playerOption = buttonOption[5];
-    computerChoice = makeComputerChoice();
+    //computerChoice = makeComputerChoice();
         console.log (playerOption);
-        console.log (computerChoice);
+        $text6.text('X');
+        //console.log (computerChoice);
 	});
 
 	$button7.on('click', function() {
 	playerOption = buttonOption[6];
-    computerChoice = makeComputerChoice();
+    //computerChoice = makeComputerChoice();
         console.log (playerOption);
-        console.log (computerChoice);
+        $text7.text('X');
+        //console.log (computerChoice);
 	});
 
 	$button8.on('click', function() {
 	playerOption = buttonOption[7];
-    computerChoice = makeComputerChoice();
+    //computerChoice = makeComputerChoice();
         console.log (playerOption);
-        console.log (computerChoice);
+        $text8.text('X');
+        //console.log (computerChoice);
 	});
 
 	$button9.on('click', function() {
 	playerOption = buttonOption[8];
-    computerChoice = makeComputerChoice();
+    //computerChoice = makeComputerChoice();
         console.log (playerOption);
-        console.log (computerChoice);
+        $text9.text('X');
+        //console.log (computerChoice);
 
 	});
 
 }
 
-function makeComputerChoice() {
-  var randomNum = Math.floor(Math.random() * choiceList.length);
-  return buttonOption[randomNum];
+function buttonEvents2() {
+    
+    $button1.on('click', function() {
+    player2Option = buttonOption[0];
+    //computerChoice = makeComputerChoice();
+        console.log (player2Option);
+        $text1.text('O');
+        //console.log (computerChoice);
+    });
+
+    $button2.on('click', function() {
+                if  (player1Turn = false) {
+    player2Option = buttonOption[1];
 }
+    //computerChoice = makeComputerChoice();
+        console.log (player2Option);
+        //console.log (computerChoice);
+    });
+
+    $button3.on('click', function() {
+    player2Option = buttonOption[2];
+    //computerChoice = makeComputerChoice();
+        console.log (player2Option);
+        //console.log (computerChoice);
+    });
+
+    $button4.on('click', function() {
+    player2Option = buttonOption[3];
+    //computerChoice = makeComputerChoice();
+        console.log (player2Option);
+        //console.log (computerChoice);
+    });
+
+    $button5.on('click', function() {
+    player2Option = buttonOption[4];
+    //computerChoice = makeComputerChoice();
+        console.log (player2Option);
+        //console.log (computerChoice);
+    });
+
+    $button6.on('click', function() {
+    player2Option = buttonOption[5];
+    //computerChoice = makeComputerChoice();
+        console.log (player2Option);
+        //console.log (computerChoice);
+    });
+
+    $button7.on('click', function() {
+    player2Option = buttonOption[6];
+    //computerChoice = makeComputerChoice();
+        console.log (player2Option);
+        //console.log (computerChoice);
+    });
+
+    $button8.on('click', function() {
+    player2Option = buttonOption[7];
+    //computerChoice = makeComputerChoice();
+        console.log (player2Option);
+        //console.log (computerChoice);
+    });
+
+    $button9.on('click', function() {
+    player2Option = buttonOption[8];
+    //computerChoice = makeComputerChoice();
+        console.log (player2Option);
+        //console.log (computerChoice);
+
+    });
+
+}
+
+init(); 
+//function makeComputerChoice() {
+  //var randomNum = Math.floor(Math.random() * buttonOption.length);
+  //return buttonOption[randomNum];
+//}
